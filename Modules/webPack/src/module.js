0e27ae7cda32448d06a8aa70fc599287c6cad9bd
Amit <amitroy9546184327@gmail.com>
2024-07-26 14:53:34 +0530

    webPack_library basics

---
+++ Modules/webPack/src/module.js
@@ -0,0 +1,7 @@
+export function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+
+
+// Bundling is the process of combining multiple JavaScript files into a single file. This is helpful because it reduces the number of files a browser needs to download, making the website load faster.
