a6a019c9d569b76da1622717db4cf4c10c02b066
Amit <amitroy9546184327@gmail.com>
2024-07-31 21:49:22 +0530

    Assertion and Boundaries

---
+++ Regular Expressions/activity_4.js
@@ -0,0 +1,17 @@
+// Task - 7
+
+let str1 =  "hello world Amit";
+let regex1 = /^\w+/;
+
+const res = str1.match(regex1)
+// console.log(res);
+
+
+
+// Taks - 8
+
+let regex2 = /\w+$/
+
+const response = str1.match(regex2)
+
+console.log(response);
